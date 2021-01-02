3c366de021404bcfaf87d033de69e2ce1ceb5d6d
dpdiliberto <deven.diliberto@mapbox.com>
2021-01-02 14:01:05 -0800

    Add comments and update CSS styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Initialize global variables
 const gallery = document.getElementById('gallery');
 let clickIndex;
 var userArray;
@@ -7,6 +8,7 @@ let modalInfoContainer;
 let searchResults;
 let isSearched = false;
 
+// Add search form
 const searchContainer = document.querySelector('.search-container');
 const searchHTML = `
     <form action="#" method="get">
@@ -15,6 +17,7 @@ const searchHTML = `
     </form>`;
 searchContainer.insertAdjacentHTML('beforeend', searchHTML);
 
+// Fetch  data
 async function fetchData(url) {
     return fetch(url)
         .then(checkStatus)
@@ -22,15 +25,18 @@ async function fetchData(url) {
         .catch(error => console.log('There was an error fetching data: ', error));
 }
 
-// fetch 12 random users from the US
+// Fetch 12 random users from the United States
 fetchData('https://randomuser.me/api/?nat=us&results=12')
     .then(data => {
 
+        // Create inital user cards and initialize hidden modal
+        // Initial fetched data is  held in the 'userArray' array
         userArray = data.results;
         createCards(userArray);
-
         initializeModal();
 
+        // Add search functionality to create new user cards that match the search query
+        // Searched data results are held in the 'searchResults' array
         searchContainer.addEventListener('submit', (e) => {
             const searchValue = e.target[0].value.toLowerCase();
             if (searchValue === '') {
@@ -51,6 +57,8 @@ fetchData('https://randomuser.me/api/?nat=us&results=12')
             }
         });
 
+        // Create modal on user card click. 
+        // Use 'searchResults' array if there was a form search or 'userArray' array if there wasn't
         gallery.addEventListener('click', (e) => {
             if (e.target.className.includes('card')) {
                 if (isSearched) {
@@ -64,13 +72,17 @@ fetchData('https://randomuser.me/api/?nat=us&results=12')
             }
         })
 
+        // Add modal interactivity
         modalContainer.addEventListener('click', (e) => {
+            
+            // Close the modal if close button is pressed
             if (e.target.className === 'modal-close-btn') {
                 modalContainer.style.display = 'none';
 
                 modalInfoContainer = document.querySelector('.modal-info-container');
                 modalInfoContainer.remove();
 
+            // getPreviousUser if the 'previous button' is clicked, and the current modal is not the first user card
             } else if (e.target.id === 'modal-prev') {
                 if (clickIndex > 0) {
                     if (isSearched) {
@@ -79,6 +91,8 @@ fetchData('https://randomuser.me/api/?nat=us&results=12')
                         toggleUser(getPreviousUser, userArray);
                     }
                 }
+
+            // getNextUser if the 'next' button is clicked, and the current modal is not the last user card
             } else if (e.target.id === 'modal-next') {
                 if (isSearched ? (clickIndex < searchResults.length - 1) : (clickIndex < userArray.length - 1)) {
                     if (isSearched) {
@@ -91,7 +105,9 @@ fetchData('https://randomuser.me/api/?nat=us&results=12')
         });
     });
 
-// throw an error if fetch response is not ok
+// ------- HELPER FUNCTIONS ------- //
+
+// Resolves or rejects depending on the promise response status
 function checkStatus(response) {
     if(response.ok) {
         return Promise.resolve(response);
@@ -100,6 +116,10 @@ function checkStatus(response) {
     }
 }
 
+/* Creates the user cards displayed in the gallery
+ *
+ * @param {array} dataArray The array containing the users to be displayed
+ */
 function createCards(dataArray) {
     let index = 0;
     dataArray.forEach(user => {
@@ -119,10 +139,18 @@ function createCards(dataArray) {
     })
 }
 
+// Remove user cards from the gallery
 function removeCards() {
     const cardDiv = document.querySelectorAll('.card');
     cardDiv.forEach(card => card.remove());
 }
+
+/* Determines which user in the array was clicked based on the selected card
+ *
+ * @param {event} e The click event
+ * @param {array} dataArray The array that the user cards reflect
+ * @return {object} selectedUser The clicked user 
+ */
 
 function getClickedUser(e, dataArray) {
     let selected = e.target;
@@ -135,25 +163,42 @@ function getClickedUser(e, dataArray) {
     return selectedUser;
 }
 
-function getPreviousUser(selectedUser, dataArray) {
+/* Determines the previous user and updates the 'selectedUser'
+ *
+ * @param {array} dataArray The array that the user cards reflect
+ * @return {object} selectedUser The previous user
+ */
+function getPreviousUser(dataArray) {
     clickIndex = parseInt(clickIndex) - 1;
     selectedUser = dataArray[clickIndex];
     return selectedUser;
 }
 
-function getNextUser(selectedUser, dataArray) {
+/* Determines the next user and updates the 'selectedUser'
+ *
+ * @param {array} dataArray The array that the user cards reflect
+ * @return {object} selectedUser The next user
+ */
+function getNextUser(dataArray) {
     clickIndex = parseInt(clickIndex) + 1;
     selectedUser = dataArray[clickIndex];
     return selectedUser;
 }
 
+/* Toggles the user by creating a new modal with the 'selectedUser'
+ *
+ * @param {function} callback The function that determines who the 'selectedUser' is
+ * @param {object} selectedUser The array that the user cards reflect
+ */
 function toggleUser(callback, dataArray) {
     modalInfoContainer = document.querySelector('.modal-info-container');
     modalInfoContainer.remove();
-    selectedUser = callback(selectedUser, dataArray);
+    selectedUser = callback(dataArray);
     createModal(selectedUser);
 }
 
+// Initializes the modal HTML so that the element can be selected but is hidden
+ 
 function initializeModal() { 
     const initializedModalHTML = `
     <div class="modal-container">
@@ -171,11 +216,14 @@ function initializeModal() {
 
 }
 
+/* Creates the new modal HTML using the 'selectedUser's data object
+ *
+ * @param {object} selectedUser The user object whose data is used to create the modal
+ */
 function createModal(selectedUser) {
 
     let cellNumber = formatCellPhone(selectedUser.cell);
     let birthday = formatBirthday(selectedUser.dob.date);
-    
     const modalHTML = `
         <div class="modal-info-container">
                 <img class="modal-img" src="${selectedUser.picture.large}" alt="profile picture">
@@ -192,14 +240,21 @@ function createModal(selectedUser) {
     modal.insertAdjacentHTML('beforeend', modalHTML);
 }
 
-// reformat cell phone number with help of https://stackoverflow.com/questions/8358084/regular-expression-to-reformat-a-us-phone-number-in-javascript
+/* Reformats the user's cell phone number 
+ * Referred to https://stackoverflow.com/questions/8358084/regular-expression-to-reformat-a-us-phone-number-in-javascript
+ *
+ * @param {string} cellNumber The user's cell number to be reformatted
+ */
 function formatCellPhone(cellNumber) {
     cellNumber = cellNumber.replace(/[^\d]+/g, '')
          .replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
     return cellNumber;
 }
 
-// reformat date of brith
+/* Reformats the user's date of birthy
+ * 
+ * @param {string} birthday The user's birthday to be reformatted
+ */
 function formatBirthday(birthday) {
     birthday = birthday.replace(/(\d{4})-(\d{2})-(\d{2}).+/, '$2/$3/$1');
     return birthday;
